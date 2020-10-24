47483ab7e5656411e783d211fcbe743e5f4a2bc1
yqisthi <73338323+yqisthi@users.noreply.github.com>
2020-10-24 10:38:57 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# alien_invasion
- 
+Alien invasion is a game I made from following tutorial on "Python Crash Course" book.
+
+# About The Game
+In Alien Invasion, the player controls a ship that appears at the bottom center of the screen.
+The player can move the ship right and left using the arrow keys and shoot bullets using the spacebar.
+When the game begins, a fleet of aliens fills the sky and moves across and down the screen.
+The player shoots and destroys the aliens. If the player shoots all the aliens,
+a new fleet appears that moves faster than the previous fleet.
+If any alien hits the player’s ship or reaches the bottom of the screen, the player loses a ship.
+If the player loses three ships, the game ends.
+
+# video game controls:
+
+shoot = space_bar
+
+Move left/right = arrow keys left/right
+
+# File name:
+alien_invasion.py to start the game
+
+alien.py alien = configuration about a single alien
+
+bullet.py = bullet config and physics
+
+button.py = button class
+
+game_function.py = how the game works
+
+game_stats.py = game status
+
+scoreboard.py = scoreboard
+
+settings.py = game settings
+
+ship.py = ship config
---
+++ alien.py
@@ -0,0 +1,36 @@
+import pygame
+from pygame.sprite import Sprite
+
+#Class to represent each alien in the game.
+class Alien(Sprite):
+
+    def __init__(self, ai_settings, screen):
+        #Initialize alien characters.
+        super(Alien, self).__init__()
+        self.screen = screen
+        self.ai_settings = ai_settings
+
+        # Load the alien image, and set its rect attribute.
+        self.image = pygame.image.load("images/alien.bmp")
+        self.rect = self.image.get_rect()
+
+        # Start each new alien near the top left of the screen.
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        # Store the alien's exact position.
+        self.x = float(self.rect.x)
+
+    def check_edges(self):
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right:
+            return True
+        elif self.rect.left <= 0:
+            return True
+
+    def update(self):
+        self.x += self.ai_settings.alien_speed_factor * self.ai_settings.fleet_direction
+        self.rect.x = self.x
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
